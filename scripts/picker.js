beee9fa55caeb00bff55405cb00de8fc6154d369
Valerie Tan <valerietan000@gmail.com>
2022-03-13 11:18:10 -0700

    buttons change color when clicked

--- scripts/picker.js
+++ scripts/picker.js
@@ -10,3 +10,14 @@ function add(clicked_id) {
     console.log(top3);
     sessionStorage.setItem("top3", top3);
 }
+
+let btn1 = document.getElementById("Okoman Restaurant");
+btn1.addEventListener('click', () => btn1.style.backgroundColor = '#337ab7');
+let btn2 = document.getElementById("Sushi California");
+btn2.addEventListener('click', () => btn2.style.backgroundColor = '#337ab7');
+let btn3 = document.getElementById("Bento Sushi");
+btn3.addEventListener('click', () => btn3.style.backgroundColor = '#337ab7');
+let btn4 = document.getElementById("North Road Sushi & Kitchen");
+btn4.addEventListener('click', () => btn4.style.backgroundColor = '#337ab7');
+let btn5 = document.getElementById("Katsuya Coquitlam");
+btn5.addEventListener('click', () => btn5.style.backgroundColor = '#337ab7');
