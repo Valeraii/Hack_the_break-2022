0f5b9578f54484d9f16a6614187c650ecf816f71
Vincent On <vincentonproduction@gmail.com>
2022-03-12 23:05:26 -0800

    made some changes

--- scripts/reduction.js
+++ scripts/reduction.js
@@ -1,21 +1,7 @@
 const list = ["apple", "banana", "pear", "orange", "strawberry", "grape", "lemon"];
 
-function reduce(list, numOptions) {
-	const reduction = [];
-	while (reduction.length < numOptions) {
-		let item = list[Math.floor(Math.random()*list.length)];
-		if (!reduction.includes(item)) {
-  		reduction.push(item);
-  		}
-	}
-  return reduction;
+function reduce(list) {
+	let index = Math.floor(Math.random() * 3);
+  	console.log(list[index]);
+	sessionStorage.setItem("final", list[index]);
 }
-
-const reduced = reduce(sessionStoreage.getItem("top3"), 5);
-console.log(reduced);
-
-document.getElementById("option1").innerHTML = reduced[0];
-document.getElementById("option2").innerHTML = reduced[1];
-document.getElementById("option3").innerHTML = reduced[2];
-document.getElementById("option4").innerHTML = reduced[3];
-document.getElementById("option5").innerHTML = reduced[4];
