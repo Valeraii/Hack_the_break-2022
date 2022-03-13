8c7642a8dfc985f0481a23b38ae707657e271d57
Vincent On <vincentonproduction@gmail.com>
2022-03-12 17:45:54 -0800

    added slider value

---
+++ scripts/reduction.js
@@ -0,0 +1,20 @@
+const list = ["apple", "banana", "pear", "orange", "strawberry", "grape", "lemon"];
+
+function reduce(list, numOptions) {
+	const reduction = [];
+	while (reduction.length < numOptions) {
+		let item = list[Math.floor(Math.random()*list.length)];
+		if (!reduction.includes(item)) {
+  		reduction.push(item);
+  	}
+	}
+  return reduction;
+}
+
+const reduced = reduce(list, 5);
+
+document.getElementById("option1").innerHTML = reduced[0];
+document.getElementById("option2").innerHTML = reduced[1];
+document.getElementById("option3").innerHTML = reduced[2];
+document.getElementById("option4").innerHTML = reduced[3];
+document.getElementById("option5").innerHTML = reduced[4];
---
+++ scripts/slider.js
@@ -0,0 +1,4 @@
+function updateSlider(slideAmount) {
+    let sliderDiv = document.getElementById("radius_numbers");
+    sliderDiv.innerHTML = slideAmount;
+}
